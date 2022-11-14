afe927aae3614d95caa95d0b57a04c830664f21f
Robert Giuga <grir2934@scs.ubbcluj.ro>
2022-11-14 20:13:58 +0200

    added login page and calendar page

--- ui/worksheet/src/app/app-routing.module.ts
+++ ui/worksheet/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AuthComponent} from "./auth/auth.component";
+import {CalendarComponent} from "./calendar/calendar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:AuthComponent},
+  {path:'calendar', component:CalendarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ui/worksheet/src/app/app.module.ts
+++ ui/worksheet/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FlexModule} from "@angular/flex-layout";
+import { AuthComponent } from './auth/auth.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
